8fc3ff4ad7ac35c232a40c7ab609003ffbe2ac06
Yash Vishnoi <88367411+YashVishnoi47@users.noreply.github.com>
2025-05-11 11:04:02 +0530

    feat: wrap body in AuthProvider for improved context management

--- frontend/app/(auth)/layout.js
+++ frontend/app/(auth)/layout.js
@@ -1,12 +1,15 @@
 import Navbar from "@/components/Navbar";
+import AuthProvider from "@/context/AuthProvider";
 
 const Layout = ({ children }) => {
   return (
     <html lang="en">
-      <body className={`w-full min-h-screen flex flex-col items-center`}>
-        <Navbar />
-        <main className="w-full h-full">{children}</main>
-      </body>
+      <AuthProvider>
+        <body className={`w-full min-h-screen flex flex-col items-center`}>
+          <Navbar />
+          <main className="w-full h-full">{children}</main>
+        </body>
+      </AuthProvider>
     </html>
   );
 };
